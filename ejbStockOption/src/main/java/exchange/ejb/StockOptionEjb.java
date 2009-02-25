7ffa7b222e2a74bcc9dc0bb1ad83b73e84fbcb48
sora <sora@shiki.(none)>
2009-02-25 15:04:36 +0100

    Add of the ejbStockOption and of the module for intellij

---
+++ ejbStockOption/src/main/java/exchange/ejb/StockOptionEjb.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2009 Anthonin Bonnefoy and David Duponchel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package exchange.ejb;
+
+import exchange.model.StockOption;
+
+import javax.ejb.Stateful;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implementation of the stock option ejb
+ */
+@Stateful
+public class StockOptionEjb implements IStockOptionEjb {
+    private List<StockOption> stockOptionList;
+
+    public StockOptionEjb() {
+        stockOptionList = new ArrayList<StockOption>();
+    }
+
+    public void createNewStockOption(StockOption stockOption) {
+        stockOptionList.add(stockOption);
+    }
+
+    public void deleteStockOption(List<StockOption> stockOption) {
+        stockOptionList.removeAll(stockOption);
+    }
+
+    public List<StockOption> getStockOptionList() {
+        return stockOptionList;
+    }
+
+    public void changesQuotes() {
+        for (StockOption stockOption : stockOptionList) {
+            double rand = Math.random() % 3;
+            float variation = (float) (Math.random() / Integer.MAX_VALUE);
+            float quote = stockOption.getQuote();
+            if (rand == 0) {
+                stockOption.setQuote(quote - variation);
+            } else if (rand == 2) {
+                stockOption.setQuote(quote + variation);
+            }
+        }
+    }
+}
